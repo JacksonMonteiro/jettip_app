aaadcc0ae03dec4353b0cb719e61ec4fd54faa84
jackson.monteiro <jackson.monteiro@thomasgreg.com.br>
2025-09-01 11:51:54 -0300

    initial commit

---
+++ app/src/main/java/br/com/jacksonmonteiro/jettip/MainActivity.kt
@@ -0,0 +1,36 @@
+package br.com.jacksonmonteiro.jettip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.jacksonmonteiro.jettip.ui.theme.JetTipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetTipTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetTipTheme {
+
+    }
+}
