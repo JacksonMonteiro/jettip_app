1401170008b24e882aeed5ebc73c7d21519386bc
jackson.monteiro <jackson.monteiro@thomasgreg.com.br>
2025-09-02 09:58:10 -0300

    InputField exhibition and refactoring and Round Icon Button widget skeleton created

--- app/src/main/java/br/com/jacksonmonteiro/jettip/MainActivity.kt
+++ app/src/main/java/br/com/jacksonmonteiro/jettip/MainActivity.kt
@@ -1,25 +1,34 @@
 package br.com.jacksonmonteiro.jettip
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +41,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApp {
-                TopHeader()
+                Column {
+                    TopHeader()
+                    MainContent()
+                }
             }
         }
     }
@@ -74,14 +86,49 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 @Preview
 @Composable
 fun MainContent() {
+    BillForm() { bill ->
+
+    }
+}
+
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValueChanged: (String) -> Unit = {}) {
+    val totalBillState = remember { mutableStateOf("") }
+    val validState = remember(totalBillState.value) { totalBillState.value.trim().isNotEmpty() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Surface(
         modifier = Modifier
             .padding(2.dp)
-            .fillMaxWidth(), shape = RoundedCornerShape(corner = CornerSize((8.dp))),
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize((8.dp))),
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
     ) {
-        Column() {
+        Column(modifier = Modifier.padding(6.dp), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start) {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Insira a conta",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValueChanged(totalBillState.value.trim())
+                    keyboardController?.hide()
+                })
 
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp), horizontalArrangement = Arrangement.Start) {
+                    Text("Dividir:", modifier = Modifier.align(alignment = Alignment.CenterVertically))
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(modifier = Modifier.padding(horizontal = 3.dp), horizontalArrangement = Arrangement.End) {
+
+                    }
+                }
+            } else {
+                Box() {}
+            }
         }
     }
 }
